e77b21b51425e96a611f4c20bd48c50406e1dae9
Muscab <muscabinho367@gmail.com>
2023-09-02 01:11:12 -0700

    added the gallery and features sections

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 
 document.querySelector(".other-links").addEventListener('click', () => {
+    
+    document.querySelector(".other-links").classList.toggle("link-active")
     document.querySelector(".mega-menu").classList.toggle("active")
 })
